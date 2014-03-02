ff3021ae24a27b4137795de5b6365cdaf0c9f7a8
ACEfanatic02 <acefanatic02@gmail.com>
2014-03-02 16:45:35 -0500

    multiply/divide

--- rbrpn/rbrpn.rb
+++ rbrpn/rbrpn.rb
@@ -16,6 +16,25 @@ class RPNCalc
     pop_stack - pop_stack
   end
 
+  def multiply
+    a = @stack.pop
+    b = @stack.pop
+
+    # Empty stack, must return 0
+    a = b = 0 if a.nil? && b.nil?
+    # Otherwise, we need to return identity of the only value
+    a = 1 if a.nil?
+    b = 1 if b.nil?
+
+    a * b
+  end
+
+  def divide
+    a = @stack.pop || 0
+    b = @stack.pop || 1
+    a / b
+  end
+
   private
 
     def pop_stack
--- spec/rbrpn_spec.rb
+++ spec/rbrpn_spec.rb
@@ -37,4 +37,40 @@ describe RPNCalc do
       calc.subtract.should eq 5
     end
   end
+
+  describe '#multiply' do
+    it 'returns the product of top arguments on stack' do
+      calc = RPNCalc.new
+      calc.push 5
+      calc.push 4
+      calc.multiply.should eq 20
+    end
+    it 'returns 0 for an empty stack' do
+      calc = RPNCalc.new
+      calc.multiply.should eq 0
+    end
+    it 'returns the only value on the stack for a single entry' do
+      calc = RPNCalc.new
+      calc.push 5
+      calc.multiply.should eq 5
+    end
+  end
+
+  describe '#divide' do
+    it 'returns the quotient of top argument on stack' do
+      calc = RPNCalc.new
+      calc.push 5
+      calc.push 4
+      calc.divide.should eq 4/5
+    end
+    it 'returns 0 for an empty stack' do
+      calc = RPNCalc.new
+      calc.divide.should eq 0
+    end
+    it 'returns the only value on the stack for a single entry' do
+      calc = RPNCalc.new
+      calc.push 5
+      calc.divide.should eq 5
+    end
+  end
 end
